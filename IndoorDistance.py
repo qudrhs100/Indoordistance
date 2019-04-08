6f7cd0c66da5908d70fa566087598e0ffb5e01a0
김병곤 <qudrhs190@gmail.com>
2019-04-08 14:37:34 +0900

    Button click

--- IndoorDistance.py
+++ IndoorDistance.py
@@ -9,7 +9,8 @@ def processCancel():
     print("Cancel button is clicekd")
 
 def click(event):
-    print("클릭위치", event.x, event.y)
+    print("Button click", event.x, event.y)
+
 
 def main():
     tree = ET.parse('TEST.gml')
@@ -17,13 +18,10 @@ def main():
 
     window = tkinter.Tk()
     window.title("Indoor Distance")
-    canvas = tkinter.Canvas(window)
-    canvas.config(width=500,height=500)
-    canvas.pack()
-    frame = Frame(window, width=500, height=500)
-    frame.bind("<Button-1>", click)
 
-    frame.pack()
+    canvas = tkinter.Canvas(window,width=500,height=500)
+    canvas.bind("<Button-1>", click)
+
     for i in root.findall("./{http://www.opengis.net/indoorgml/1.0/core}primalSpaceFeatures/{http://www.opengis.net/indoorgml/1.0/core}PrimalSpaceFeatures/{http://www.opengis.net/indoorgml/1.0/core}cellSpaceMember/{http://www.opengis.net/indoorgml/1.0/core}CellSpace/{http://www.opengis.net/indoorgml/1.0/core}cellSpaceGeometry/{http://www.opengis.net/indoorgml/1.0/core}Geometry2D/{http://www.opengis.net/gml/3.2}Polygon/{http://www.opengis.net/gml/3.2}exterior/{http://www.opengis.net/gml/3.2}LinearRing"):
         list=[]
         for j in i.findall("./{http://www.opengis.net/gml/3.2}pos"):
@@ -31,6 +29,7 @@ def main():
             list.append(str(float(words[0])))
             list.append(str(float(words[1])))
         canvas.create_polygon(list, outline='black', fill='ivory3', width=2)
+    canvas.pack()
     window.mainloop()
 
 if __name__ == "__main__":
