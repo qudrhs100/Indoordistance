75f87b05b276932cd03e2b0c3ce545eef9ead8a1
김병곤 <qudrhs190@gmail.com>
2019-04-09 10:48:09 +0900

    Line intersection check

--- IndoorDistance.py
+++ IndoorDistance.py
@@ -1,5 +1,7 @@
 import xml.etree.ElementTree as ET
 import tkinter
+import networkx as nx
+
 from tkinter import *
 from PIL import ImageTk, Image
 
@@ -9,11 +11,23 @@ root = tree.getroot()
 window = tkinter.Tk()
 window.title("Indoor Distance")
 canvas = tkinter.Canvas(window, width=500, height=500)
+
 img = ImageTk.PhotoImage(Image.open("Door.png"))
 total_click = 0
+class Point:
+	def __init__(self,x,y):
+		self.x = x
+		self.y = y
+
+def ccw(A,B,C):
+    return (C.y-A.y) * (B.x-A.x) > (B.y-A.y) * (C.x-A.x)
+
+def intersect(A,B,C,D):
+    return ccw(A,C,D) != ccw(B,C,D) and ccw(A,B,C) != ccw(A,B,D)
 
 def calculate_path():
     print("Caculatepath")
+
 def processOK():
     print("OK button is clicked")
 
@@ -52,7 +66,14 @@ def main():
             sum_y+=float(words[1])
         canvas.create_image(sum_x/2-15, sum_y/2-15, image=img, anchor=NW)
 
+    a = Point(0, 0)
+    b = Point(0, 1)
+    c = Point(1, 1)
+    d = Point(1, 0)
 
+    print (intersect(a, b, c, d))
+    print (intersect(a, c, b, d))
+    print (intersect(a, d, b, c))
     canvas.pack()
     btn.pack()
     window.mainloop()
